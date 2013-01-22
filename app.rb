e5068a3fc0dfbba43c61a467060f51ef838c53d6
Ellen Gummesson <ellengummesson@gmail.com>
2013-01-22 21:21:55 +0100

    Document all the things!

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
-# Gem/lib requirements
+# Gems
 require 'rubygems'
 require 'sinatra'
+
+# Standard Library
 require 'date'
 
+# Information
 APP_NAME = "Slugize"
 APP_DESC = "Go ahead, make a slug."
 
 # Set app directory as root
 $:.unshift File.dirname(__FILE__)
 
-# App requirements
+# App Library
 require 'lib/helpers'
 require 'lib/routes'
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,16 +1,27 @@
 helpers do
-  # Transforms the title into a slug by removing ', ! and ?
-  # and replacing whitespace with -
+  # Parses the title into a slug
+  #
+  #  title = The given title. Transforms the title into a slug by removing 
+  #          ', !, ?, :, replacing the whitespace with - and changing it to
+  #          downcase.
+  #
   def slugize(title)
     "#{title}".gsub(/(\'|\!|\?|\:|\s\z)/,"").gsub(/\s/,"-").downcase
   end
 
-  # Parse date
+  # Parses the date
+  #
+  #  today = yyyy-mm-dd
+  #
   def date
     Date.today
   end
 
-  # Display message
+  # Displays a message
+  #
+  #  message = The given message. Redirects to another page with a different
+  #            layout to display the message.
+  #
   def message(message)
     @message = message
     erb :message
